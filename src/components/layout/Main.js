8cc2a4f7998d686ed5cbb0dc79dbd503b58d10eb
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-04-28 15:31:43 +0900

    fix: 진료페이지

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,21 +1,8 @@
-/*!
-  =========================================================
-  * Muse Ant Design Dashboard - v1.0.0
-  =========================================================
-  * Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
-  * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-  * Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
-  * Coded by Creative Tim
-  =========================================================
-  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Layout, Drawer, Affix } from "antd";
 import Sidenav from "./Sidenav";
 import Header from "./Header";
-import Footer from "./Footer";
 
 const { Header: AntHeader, Content, Sider } = Layout;
 
@@ -27,9 +14,6 @@ function Main({ children }) {
   const [fixed, setFixed] = useState(false);
 
   const openDrawer = () => setVisible(!visible);
-  const handleSidenavType = (type) => setSidenavType(type);
-  const handleSidenavColor = (color) => setSidenavColor(color);
-  const handleFixedNavbar = (type) => setFixed(type);
 
   let { pathname } = useLocation();
   pathname = pathname.replace("/", "");
@@ -43,85 +27,43 @@ function Main({ children }) {
   }, [pathname]);
 
   return (
-    <Layout
-      className={`layout-dashboard ${
-        pathname === "profile" ? "layout-profile" : ""
-      } ${pathname === "rtl" ? "layout-dashboard-rtl" : ""}`}
-    >
+    <Layout className={`layout-dashboard`}>
       <Drawer
         title={false}
-        placement={placement === "right" ? "left" : "right"}
-        closable={false}
-        onClose={() => setVisible(false)}
-        visible={visible}
-        key={placement === "right" ? "left" : "right"}
+        placement={"left"}
+        key={"left"}
         width={250}
-        className={`drawer-sidebar ${
-          pathname === "rtl" ? "drawer-sidebar-rtl" : ""
-        } `}
+        className={`drawer-sidebar`}
       >
-        <Layout
-          className={`layout-dashboard ${
-            pathname === "rtl" ? "layout-dashboard-rtl" : ""
-          }`}
-        >
+        <Layout className={`layout-dashboard`}>
           <Sider
-            trigger={null}
-            width={250}
             theme="light"
-            className={`sider-primary ant-layout-sider-primary ${
-              sidenavType === "#fff" ? "active-route" : ""
-            }`}
-            style={{ background: sidenavType }}
+            className={`sider-primary ant-layout-sider-primary`}
+            style={{ background: "transparent" }}
           >
-            <Sidenav color={sidenavColor} />
+            <Sidenav color={"#1890ff"} />
           </Sider>
         </Layout>
       </Drawer>
       <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-        trigger={null}
         width={250}
         theme="light"
-        className={`sider-primary ant-layout-sider-primary ${
-          sidenavType === "#fff" ? "active-route" : ""
-        }`}
-        style={{ background: sidenavType }}
+        className={`sider-primary ant-layout-sider-primary`}
+        style={{ background: "transparent" }}
       >
-        <Sidenav color={sidenavColor} />
+        <Sidenav color={"#1890ff"} />
       </Sider>
       <Layout>
-        {fixed ? (
           <Affix>
-            <AntHeader className={`${fixed ? "ant-header-fixed" : ""}`}>
+            <AntHeader className={`ant-header-fixed`}>
               <Header
                 onPress={openDrawer}
                 name={pathname}
                 subName={pathname}
-                handleSidenavColor={handleSidenavColor}
-                handleSidenavType={handleSidenavType}
-                handleFixedNavbar={handleFixedNavbar}
               />
             </AntHeader>
           </Affix>
-        ) : (
-          <AntHeader className={`${fixed ? "ant-header-fixed" : ""}`}>
-            <Header
-              onPress={openDrawer}
-              name={pathname}
-              subName={pathname}
-              handleSidenavColor={handleSidenavColor}
-              handleSidenavType={handleSidenavType}
-              handleFixedNavbar={handleFixedNavbar}
-            />
-          </AntHeader>
-        )}
         <Content className="content-ant">{children}</Content>
-        <Footer />
       </Layout>
     </Layout>
   );
