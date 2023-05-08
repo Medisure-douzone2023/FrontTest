0801f03bb4acf99d45fd149c5727ed9b4898604f
kimki0917 <kimki0917@naver.com>
2023-05-08 19:25:47 +0900

    feat: biil, insertmanual 정리, dbclick clear

--- src/pages/Bill/Bill.js
+++ src/pages/Bill/Bill.js
@@ -3,39 +3,17 @@ import { Button, Table, DatePicker, Space, Select, Row, Col } from 'antd';
 import axios from 'axios'
 
 const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0IiwicG9zaXRpb24iOiJvZmZpY2UiLCJpYXQiOjE2ODM1MDg3NzQsImV4cCI6MTY4MzgwODc3NH0.TvJFxxZR_3ALICx94iaFFQY6tglxfAO2-rySDxq049g';
-let searchDate = '';
-let searchOption = '미송신';
-let apiArray = {};
 
-function Home() {
+function Bill() {
+
+  const statusOptions = [{ value: '미송신', labe: '미송신' }, { value: '변환', labe: '변환' }];
+  const insuranceOptions = [{ value: '건강보험', labe: '건강보험' }, { value: '의료급여', labe: '의료급여' }];
+
+  const [date, setDate] = useState('');
+  const [insurance, setInsurance] = useState('건강보험');
+  const [status, setStatus] = useState('미송신');
   const [size, setSize] = useState('middle');
-  let [data, setData] = useState([]);
-  const setDate = (value, dateString) => {
-    searchDate = dateString
 
-  }
-
-  const options = [{ value: '미송신', labe: '미송신' }, { value: '변환', labe: '변환' }];
-  const setOption = (value, option) => {
-    searchOption = value;
-  };
-
-
-  const search = (value, dateString) => {
-
-    const param = { month: searchDate, status: searchOption };
-      axios.get("/api/bill", { headers: { "Authorization": token }, params: param }).then((e) => {
-      const result = e.data.data;
-      for (var i = 0; i < result.length; i++) {
-        result[i].key = i;
-      }
-      setData(result)
-    }).catch(() => {
-    })
-  }
-
-  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-  const [loading, setLoading] = useState(false);
   const columns = [
     {
       title: '청구번호',
@@ -83,6 +61,33 @@ function Home() {
       key: 'week',
     }
   ];
+  let [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const selectDate = (value, dateString) => {
+    setDate(dateString);
+  }
+  const selectStatus = (value, option) => {
+    setStatus(value);
+  };
+  const setInsuranceOption = (value, option) => {
+    setInsurance(value);
+  };
+
+  const search = () => {
+    const param = { month: date, insurance: insurance, status: status };
+    axios.get("/api/bill", { headers: { "Authorization": token }, params: param }).then((e) => {
+      const result = e.data.data;
+      for (var i = 0; i < result.length; i++) {
+        result[i].key = i;
+      }
+      setData(result)
+    }).catch((e) => {
+      console.log("error", e);
+    })
+  }
 
   const start = () => {
     setLoading(true);
@@ -92,7 +97,7 @@ function Home() {
     }, 1000);
   };
   const onSelectChange = (newSelectedRowKeys, selectedRows) => {
-    apiArray = selectedRows;
+    setSelectedRows(selectedRows)
     setSelectedRowKeys(newSelectedRowKeys);
   };
   const rowSelection = {
@@ -102,39 +107,51 @@ function Home() {
   const hasSelected = selectedRowKeys.length > 0;
 
   const send = () => {
-    let apiParameters = [];
-    for (let i = 0; i < apiArray.length; i++) {
-      apiParameters.push(apiArray[i].bno)
+    if (selectedRows.length > 0) {
+      let apiParameters = [];
+      for (let i = 0; i < selectedRows.length; i++) {
+        apiParameters.push(selectedRows[i].bno)
+      }
+      const result = data.filter(d => !selectedRows.some(s => d.key === s.key));
+      setData(result)
+      setSelectedRows('');
+      setSelectedRowKeys('');
+
+      axios.put("/api/bill/make", apiParameters, {
+        headers: {
+          "Authorization": token,
+        },
+      }).then((response) => {
+        alert("sam 파일 생성이 완료되었습니다.")
+      }).catch((e) => {
+        console.log("error", e);
+      });
     }
-    axios.put("/api/bill/make", apiParameters, {
-      headers: {
-        "Authorization": token,
-      },
-    }).then((response) => {
-      search(searchDate, searchDate)
-      alert("sam 파일 생성이 완료되었습니다.")
-    }).catch((error) => {
-    });
   }
 
-  const cancel = () =>{
-    console.log("송신취소 apiArray",apiArray)
-    let apiParams =[];
-    for (let i = 0; i < apiArray.length; i++) {
-      apiParams.push(apiArray[i].bno)
+  const cancel = () => {
+    if (selectedRows.length > 0) {
+      let apiParameters = [];
+      console.log("apiParameters1", apiParameters);
+      for (let i = 0; i < selectedRows.length; i++) {
+        apiParameters.push(selectedRows[i].bno)
+      }
+
+      const result = data.filter(d => !selectedRows.some(s => d.key === s.key));
+      setData(result)
+      setSelectedRows('');
+      setSelectedRowKeys('')
+
+      axios.put("/api/bill/delete", apiParameters, {
+        headers: {
+          "Authorization": token,
+        },
+      }).then((response) => {
+        alert("sam 파일 삭제가 완료되었습니다.")
+      }).catch((e) => {
+        console.log("error", e);
+      });
     }
-    console.log(apiParams)
-    axios.put("/api/bill/delete", apiParams, {
-      headers: {
-        "Authorization": token,
-      },
-    }).then((response) => {
-      search(searchDate, searchDate)
-      alert("sam 파일 삭제가 완료되었습니다.")
-    }).catch((error) => {
-    });
-
-
   }
   return (
     <div>
@@ -142,37 +159,29 @@ function Home() {
       <h4>청구서 검색</h4>
       <br />
       <Row>
-        <Col lg={{ span: 1, offset: 1 }}>
-          진료년월
-        </Col>
-        <Col xs={{ span: 1, offset: 1 }} lg={{ span: 5, offset: 1 }}>
-          <Space direction="vertical" size={12}>
-            <DatePicker picker="month" onChange={setDate} />
-          </Space>
-        </Col>
-        <Col xs={{ span: 1, offset: 1 }} lg={{ span: 1, offset: 1 }}>
-          상태
-        </Col>
-        <Col xs={{ span: 1, offset: 1 }} lg={{ span: 1, offset: 1 }}>
-          <Space
-            direction="vertical"
-            style={{
-              width: '100%',
-            }}
-          >
+        <Col>
+          <Space direction="horizontal" size={12}>
+            <DatePicker picker="month" onChange={selectDate} />
             <Select
               size={size}
-              defaultValue="미송신"
-              onChange={setOption}
+              defaultValue="건강보험"
+              onChange={setInsuranceOption}
               style={{
                 width: 200,
               }}
-              options={options}
+              options={insuranceOptions}
             />
+            <Select
+              size={size}
+              defaultValue="미송신"
+              onChange={selectStatus}
+              style={{
+                width: 200,
+              }}
+              options={statusOptions}
+            />
+            <Button type="primary" ghost onClick={search}>검색</Button>
           </Space>
-        </Col>
-        <Col xs={{ span: 1, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-          <Button type="primary" ghost onClick={search}>검색</Button>
         </Col>
       </Row>
       <br />
@@ -198,5 +207,5 @@ function Home() {
     </div>
   );
 }
-export default Home;
+export default Bill;
 
--- src/pages/InsertManual/InsertManual.js
+++ src/pages/InsertManual/InsertManual.js
@@ -1,79 +1,88 @@
-import { Table, DatePicker, Space, Button } from "antd";
+import { Table, DatePicker, Space, Button, Select, Row, Col } from "antd";
 import axios from 'axios'
 import { useState } from "react";
 import '../../assets/styles/InsertManual.css';
 
-function Billing() {
+let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0IiwicG9zaXRpb24iOiJvZmZpY2UiLCJpYXQiOjE2ODM1MTAwNTksImV4cCI6MTY4MzgxMDA1OX0.4jXsvwp3Kfw-JFE3-chMiNKZtlQxXXoOR7VHDST5KH4';
+
+function InsertManual() {
+
+  const options = [{ value: '건강보험', labe: '건강보험' }, { value: '의료급여', labe: '의료급여' }];
   const { RangePicker } = DatePicker;
-  let [dataSource, setDataSource] = useState([]);
-  let apiArray = {};
+  const [date, setDate] = useState('');
+  const [insurance, setInsurance] = useState('건강보험');
+  const [size, setSize] = useState('middle');
 
-  const apiParam = () => {
-    let apiParameters = [];
-    for (let i = 0; i < apiArray.length; i++) {
-      apiParameters.push([apiArray[i].rno, apiArray[i].pno ])
+  const columns = [
+    {
+      title: '환자명',
+      dataIndex: 'pname',
+      key: 'pname',
+    },
+    {
+      title: '나이',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '성별',
+      dataIndex: 'gender',
+      key: 'gender',
+    },
+    {
+      title: '보험유형',
+      dataIndex: 'insurance',
+      key: 'insurance',
+    },
+    {
+      title: '진료기간',
+      dataIndex: 'rdate',
+      key: 'rdate',
+    },
+    {
+      title: '본인부담금',
+      dataIndex: 'fprice',
+      key: 'fprice',
+    },
+    {
+      title: '청구금액',
+      dataIndex: 'totalprice',
+      key: 'totalprice',
+    },
+    {
+      title: '초진여부',
+      dataIndex: 'visit',
+      key: 'visit',
+    }, {
+      title: '상태',
+      dataIndex: 'status',
+      key: 'status',
     }
-    let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0IiwicG9zaXRpb24iOiJvZmZpY2UiLCJpYXQiOjE2ODM1MDQ2MjQsImV4cCI6MTY4MzgwNDYyNH0.ishfC37fiyigb6H59LfHmUpgsMQnzV9JT18hccoVFVU';
-    axios.post("/api/bill/manualInsert",apiParameters, {
-      headers: {
-        "Authorization": token,
-      },
-    })
-    .then((response) => {
-      const result = dataSource.filter(data => !apiArray.some(apry => data.key === apry.key));
-      setDataSource(result)
-      alert("수동생성이 완료되었습니다.")
-    }).catch((error) => {});
-  }
+  ];
 
-const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-const [loading, setLoading] = useState(false);
-const columns = [
-  {
-    title: '환자명',
-    dataIndex: 'pname',
-    key: 'pname',
-  },
-  {
-    title: '나이',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: '성별',
-    dataIndex: 'gender',
-    key: 'gender',
-  },
-  {
-    title: '보험유형',
-    dataIndex: 'insurance',
-    key: 'insurance',
-  },
-  {
-    title: '진료기간',
-    dataIndex: 'rdate',
-    key: 'rdate',
-  },
-  {
-    title: '본인부담금',
-    dataIndex: 'fprice',
-    key: 'fprice',
-  },
-  {
-    title: '청구금액',
-    dataIndex: 'totalprice',
-    key: 'totalprice',
-  },
-  {
-    title: '초진여부',
-    dataIndex: 'visit',
-    key: 'visit',
-  }, {
-    title: '상태',
-    dataIndex: 'status',
-    key: 'status',
+  let [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const selectDate = (value, dateString) => {
+    setDate(dateString);
   }
-];
+  const handleChange = (value) => {
+    setInsurance(value);
+  };
+
+  const search = () => {
+    const param = { startDate: date[0], endDate: date[1], insurance: insurance };
+    axios.get("/api/receipt/insertManual", { headers: { "Authorization": token }, params: param }
+    ).then((response) => {
+      const result = response.data.data;
+      for (let i = 0; i < result.length; i++) {
+        result[i].key = i;
+      }
+      setDataSource(result);
+    }).catch((e) => { console.log("error", e); });
+  }
 
   const start = () => {
     setLoading(true);
@@ -83,54 +92,74 @@ const columns = [
     }, 1000);
   };
 
+  const onSelectChange = (newSelectedRowKeys, selectedRows) => {
+    setSelectedRowKeys(newSelectedRowKeys);
+    setSelectedRows(selectedRows);
+  };
   const rowSelection = {
-    onChange: (selectedRowKeys, selectedRows) => {
-      apiArray = selectedRows;
-      },
-    getCheckboxProps: (record) => ({
-      disabled: record.name === 'Disabled User',
-      name: record.name,
-    }),
+    selectedRowKeys,
+    onChange: onSelectChange,
   };
   const hasSelected = selectedRowKeys.length > 0;
+
+  const apiParam = () => {
+    if (selectedRows.length > 0) {
+      let apiParameters = [];
+      const result = dataSource.filter(data => !selectedRows.some(apry => data.key === apry.key));
+      setDataSource(result)
+      for (let i = 0; i < selectedRows.length; i++) {
+        apiParameters.push([selectedRows[i].rno, selectedRows[i].pno])
+      }
+      setSelectedRows('');
+      setSelectedRowKeys('');
+      axios.post("/api/bill/manualInsert", apiParameters, {
+        headers: {
+          "Authorization": token,
+        },
+      })
+        .then((response) => {
+          alert("수동생성이 완료되었습니다.")
+        }).catch((e) => { console.log("error", e); });
+    }
+  }
 
   return (
     <div>
       <h4> 진료 검색 </h4>
-      <Space direction="vertical" size={12}>
-        <RangePicker onChange={(value, dateString) => {
-          const param = { startDate: dateString[0], endDate: dateString[1] };
-          let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0IiwicG9zaXRpb24iOiJvZmZpY2UiLCJpYXQiOjE2ODMxOTAzMTMsImV4cCI6MTY4MzQ5MDMxM30.eTGjLxJITDxQ_L38jY2WpsF_lSy_rST3t1ikFsuNRAc';
-          axios.get("/api/receipt/insertManual", { headers: { "Authorization": token }, params: param }
-          ).then((response) => {
-            const result = response.data.data;
-            for (let i = 0; i < result.length; i++) {
-              result[i].key = i;
-            }
-            setDataSource(result);
-          }).catch((error) => {});
-        }} />
-      </Space>
+      <Row>
+        <Col>
+          <Space direction="horizontal" size={12}>
+            <RangePicker onChange={selectDate} />
+            <Select
+              size={size}
+              defaultValue="건강보험"
+              onChange={handleChange}
+              style={{ width: 200 }}
+              options={options}
+            />
+            <Button type="primary" ghost onClick={search}>검색</Button>
+          </Space>
+        </Col>
+      </Row>
       <br />
       <br />
-      <h4> 결과 조회 </h4>
+      <h4> 진료 조회 </h4>
       <br />
       <div>
         <div
-          style={{ marginBottom: 16}}>
+          style={{ marginBottom: 16 }}>
           <Button type="primary" onClick={start} disabled={!hasSelected} loading={loading}>
             Reload
           </Button>
           <span
-            style={{marginLeft: 8}}>
+            style={{ marginLeft: 8 }}>
             {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
           </span>
         </div>
         <Table rowSelection={rowSelection} columns={columns} dataSource={dataSource} />
       </div>
-      <Button type="primary" ghost onClick={apiParam}>수동생성</Button>
+      <Button type="primary" ghost onClick={apiParam} disabled={!hasSelected}>수동생성</Button>
     </div>
   );
 }
-//분할,css
-export default Billing;
+export default InsertManual;
