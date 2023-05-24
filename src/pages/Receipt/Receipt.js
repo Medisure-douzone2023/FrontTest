f45b4233f176a3948483f738f5f022869faa0de4
castlebear2 <tjddnd5729@gmail.com>
2023-05-24 14:34:08 +0900

    feat: receiptCharts, feeList, newPatientRegister, etc

--- src/pages/Receipt/Receipt.js
+++ src/pages/Receipt/Receipt.js
@@ -1,15 +1,33 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios'
-import { Row, Col } from "antd";
+import axios from 'axios';
+import { Row, Col, Card } from "antd";
 import '../../assets/styles/Receipt.css';
 import ReceiptStatus from './ReceiptStatus';
 import PatientSearch from './PatientSearch';
 import FeeList from './FeeList';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'green'];
 
 function Receipt(props) {
   let token = props.token;
+
+  const [totalCount, setTotalCount] = useState(0);
+  const [receiptCount, setReceiptCount] = useState(0);
+  const [careCount, setCareCount] = useState(0);
+  const [feeCount, setFeeCount] = useState(0);
+  const [completeCount, setCompleteCount] = useState(0);
+  // charts용 데이터
+  const chartdata = [
+    { name: '전체', value: totalCount} ,
+    { name: '접수', value: receiptCount},
+    { name: '진료중', value: careCount},
+    { name: '수납대기', value: feeCount},
+    { name: '완료', value: completeCount},
+  ];
+
+
+
   const [feeTableData, setFeeTableData] = useState([]);
-  // 수납 테이블 리스트 데이터 가져오는 함수
   const fetchFeeTableData = () => {
     axios.get('/api/fee/list', {
       headers: {
@@ -18,25 +36,117 @@ function Receipt(props) {
     })
       .then((response) => {
         setFeeTableData(response.data.data);
-        // console.log("feeTableData", response.data.data);
       })
       .catch((error) => {
-        // console.log("feeTableData is here : ", feeTableData);
         console.log(error);
       });
-  }
+  };
+
   const [receiptData, setReceiptData] = useState([]);
   const [status, setStatus] = useState('전체');
+
   useEffect(() => {
     fetchReceiptData();
   }, [status]);
 
   useEffect(() => {
+    resetAllCount();
+  }, []);
+
+  useEffect(() => {
     setReceiptData(receiptData);
   }, [receiptData]);
-  // 환자 상태에 따른, 접수 테이블 데이터 가져오기 
+
+  const resetAllCount = () => {
+    fetchTotalCount();
+    fetchReceiptCount();
+    fetchCareCount();
+    fetchFeeCount();
+    fetchCompleteCount();
+  };
+
+  const fetchTotalCount = async () => {
+    await axios.get('/api/receipt/status', {
+      headers: {
+        "Authorization": props.token
+      },
+      params: {
+        status: "전체"
+      }
+    })
+      .then((response) => {
+        setTotalCount(response.data.data.length);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const fetchReceiptCount = async () => {
+    await axios.get('/api/receipt/status', {
+      headers: {
+        "Authorization": props.token
+      },
+      params: {
+        status: "접수"
+      } 
+    })
+      .then((response) => {
+        setReceiptCount(response.data.data.length);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const fetchCareCount = async () => {
+    await axios.get('/api/receipt/status', {
+      headers: {
+        "Authorization": props.token
+      },
+      params: {
+        status: "진료중"
+      }
+    })
+      .then((response) => {
+        setCareCount(response.data.data.length);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const fetchFeeCount = async () => {
+    await axios.get('/api/receipt/status', {
+      headers: {
+        "Authorization": props.token
+      },
+      params: {
+        status: "수납대기"
+      }
+    })
+      .then((response) => {
+        setFeeCount(response.data.data.length);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const fetchCompleteCount = async () => {
+    await axios.get('/api/receipt/status', {
+      headers: { "Authorization": props.token },
+      params: { status: "완료" }
+    })
+      .then((response) => {
+        setCompleteCount(response.data.data.length);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   const fetchReceiptData = async () => {
-    // setReceiptData([]);
     await axios.get('/api/receipt/status', {
       headers: {
         "Authorization": props.token
@@ -51,38 +161,144 @@ function Receipt(props) {
           const localDate = date.toLocaleString().split(".")[3].slice(0, date.toLocaleString().split(".")[3].length - 3);
           data.rdate = localDate;
 
-          const visit = data.visit === "y" ? "재진" : "초진";
+          const visit = data.visit === "Y" ? "재진" : "초진";
           data.visit = visit;
 
-          const pay = data.pay === "y" ? "완료" : "미납";
+          const pay = data.pay === "Y" ? "완료" : "미납";
           data.pay = pay;
-
-        })
+        });
         setReceiptData(response.data.data);
-        // console.log("receiptData", receiptData); 
       })
       .catch((error) => {
-        console.log(error); 
+        console.log(error);
       });
+  };
+
+  const renderActiveShape = (props) => {
+    const RADIAN = Math.PI / 180;
+    const { cx, cy, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
+    const midAngle = (startAngle + endAngle) / 2;
+
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? 'start' : 'end';
+    return (
+      <g>
+
+        {/* 차트 중간에 제목 데이터 출력 코드 */}
+        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill} fontSize={39} >
+          {payload.name}
+        </text>
+
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius} 
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}               // sector 색깔임.
+        />
+        {/* 이거 두번째코드임 origin 아님.*/}
+        {/* <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+        </text> */}
+
+        {/* Sector: 마우스 오버했을 때 차트 외곽에 선으로 나타내는 거 */}
+        <Sector
+          cx={cx}
+          cy={cy}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          innerRadius={outerRadius + 7}   
+          outerRadius={outerRadius + 12}
+          fill={fill}
+        />
+        <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+        
+        {/* 작은 구슬 */}
+        <circle cx={ex} cy={ey} r={4} fill={fill} stroke="none" /> 
+        
+        {/* 위에 짝대기 해서 뜨는 글자 (마우스 오버했을때) */}
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`${value}건`}</text>
+        {/* 회색 작은 퍼센트 글자 */} 
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={20} textAnchor={textAnchor} fill="#999"> 
+          {`(Rate ${(percent * 100).toFixed(0)}%)`}
+        </text>
+      </g>
+    );
+  };
+  // label 용 함수 (defaul로 다 데이터 띄우려고 함)
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const RADIAN = Math.PI / 180;
+
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.39;
+      const x = cx + radius * Math.cos(-midAngle * RADIAN);
+      const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    return (
+      // 여기가 5개의 데이터 숫자 다 띄워주는 text, 출력시키고 싶은 데이터 바인딩하면 됨.
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} fontSize={22} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onPieEnter = (_, index) => {
+    setActiveIndex(index);
   };
 
 
   return (
     <>
-      {/* 1행 - 환자리스트 + 검색창 */}
-      <Row gutter={[28, 12]}  >
-        <Col xs={14} sm={16} md={18} lg={21} xl={24}>
-          <PatientSearch token={token}
+      <Row gutter={[24, 24]}>
+        <Col xs={16} sm={16} md={18} lg={18} xl={16}>
+          <PatientSearch
+            token={token}
+            resetAllCount={resetAllCount}
             status={status}
             receiptData={receiptData}
-            fetchReceiptData={fetchReceiptData} />
+            fetchReceiptData={fetchReceiptData}
+          />
+        </Col>
+        <Col xs={8} sm={8} md={6} lg={4} xl={8}>
+          <Card className='chartcard'>
+            <PieChart width={675} height={650}>
+              <Pie
+                activeIndex={activeIndex}
+                activeShape={renderActiveShape}
+                data={chartdata}
+                cx="40%"           // 화면상 x 좌표
+                cy="40%"           // 화면상 y 좌표
+                innerRadius={120}  // 크기 조정 안쪽원
+                outerRadius={200}  // 크기 조정 바깥원
+                fill="#5996F8"     // 색상
+                dataKey="value"
+                onMouseEnter={onPieEnter} // 마우스 올렸을 때 responsive
+                isAnimationActive={true}  // 애니메이션
+                labelLine={false}         // 더듬이 없애기
+                label={renderCustomizedLabel} // 라벨용 (전체 데이터 띄워주기)
+              >
+                {/* 차트에 색깔 입혀주는 거 */}
+                {chartdata.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+            </PieChart>
+          </Card>
         </Col>
       </Row>
-      {/* 2행 - 환자상태 테이블 & 수납테이블 */}
-      <Row gutter={[16, 0]}  > {/* 두 테이블 사이 간격 조절 */}
-        {/* 환자상태 테이블 */}
-        <Col xs={14} sm={14} md={14} lg={14} xl={14} >
-          <ReceiptStatus token={token}
+
+      <Row gutter={[16, 0]}>
+        <Col xs={14} sm={14} md={14} lg={14} xl={14}>
+          <ReceiptStatus
+            token={token}
             status={status}
             setStatus={setStatus}
             receiptData={receiptData}
@@ -90,20 +306,27 @@ function Receipt(props) {
             fetchReceiptData={fetchReceiptData}
             fetchFeeTableData={fetchFeeTableData}
             feeTableData={feeTableData}
+            resetAllCount={resetAllCount}
+            totalCount={totalCount}
+            receiptCount={receiptCount}
+            careCount={careCount}
+            feeCount={feeCount}
+            completeCount={completeCount}
           />
         </Col>
-        {/* 수납테이블 */}
-        <Col xs={10} sm={10} md={10} lg={10} xl={10} >
-          <FeeList token={token}
+        <Col xs={10} sm={10} md={10} lg={10} xl={10}>
+          <FeeList
+            token={token}
+            resetAllCount={resetAllCount}
             fetchFeeTableData={fetchFeeTableData}
             feeTableData={feeTableData}
             status={status}
-            fetchReceiptData={fetchReceiptData} />
+            fetchReceiptData={fetchReceiptData}
+          />
         </Col>
       </Row>
-
     </>
   );
 }
 
-export default Receipt; 
+export default Receipt;
